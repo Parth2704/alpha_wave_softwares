34086751a828c16e518eead5b3750e2e60e7c2db
Parth Khunt <khunt8533@gmail.com>
2025-09-11 17:58:15 +0530

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -85,7 +85,7 @@ const Contact = () => {
       icon: <Phone />,
       title: 'Call Us',
       details: '+91 9157877809',
-      subtitle: 'Mon-Fri 9AM-6PM EST',
+      subtitle: 'Mon-Fri 9 AM - 6 PM',
     },
     // {
     //   icon: <LocationOn />,
